8ad58d612a2110abd160bc9336ddd001d0667094
ryo1925-jp <ryo1925@gmail.com>
2026-01-30 14:09:04 +0900

    ビルド＆dist削除

--- src/data/initialData.ts
+++ src/data/initialData.ts
@@ -1,5 +1,5 @@
 import type { DataStore } from '../types';
-import categoriesData from './categories.json';
+import categoriesData from '../../初期データ.json';
 
 // Type assertion is needed because JSON imports are treated as inferred types
 export const initialData: DataStore = categoriesData as unknown as DataStore;
